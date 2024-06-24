83df914f495c8a3d160b3a12a09326318ab49b94
Borges Costta <borgesdevfront@gmail.com>
2024-06-24 10:24:22 -0300

    Add class on variables.

---
+++ variaveis.py
@@ -0,0 +1,25 @@
+name = "Borges"
+print(name)
+
+media = 7
+n1 = n2= n3 = 0.0
+nome, idade = "José", 26
+status = True
+
+# Função type()
+print(type(media))
+print(type(nome))
+print(type(n1))
+print(type(status))
+print(type(1 + 2j))
+
+# Função isinstance()
+a = 10
+b = "Sol"
+print(isinstance(a, float))
+print(isinstance(b, str))
+
+c = 40
+d = 3
+result = c * d
+print(result)
